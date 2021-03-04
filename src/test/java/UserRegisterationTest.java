ca18e7f3880be9a6804dcc517cc373f2010ba11d
RachanaBR-jain <br19rachanajain@gmail.com>
2021-03-04 09:04:23 +0530

    Added Code to Validate FirstName ,LastName,MobileNumber ,Email,Passwaord

--- src/test/java/UserRegisterationTest.java
+++ src/test/java/UserRegisterationTest.java
@@ -10,46 +10,59 @@ public class UserRegisterationTest {
             user = new UserRegistration();
         }
 
+        @Test
+        public void firstName_WhenTrue() {
+        boolean result = user.validateFirstName("Rachanaa");
+        Assertions.assertTrue(result);
 
-        /* @Test
+        }
+
+        @Test
+        public void firstName_WhenFalse() {
+        boolean result = user.validateFirstName("Ra^^G8980909099");
+        Assertions.assertFalse(result);
+
+        }
+
+        @Test
         public void lastName_WhenTrue() {
              boolean result = user.validateLastName("Jain");
-             Assertions.assertEquals(true, result);
+             Assertions.assertTrue(result);
 
          }
 
         @Test
         public void lastName_WhenFalse() {
             boolean result = user.validateLastName("Jain$^GG");
-            Assertions.assertEquals(false, result);
+            Assertions.assertFalse(result);
+        }
 
-
-    @Test
-    public void email_WhenTrue() {
+        @Test
+         public void email_WhenTrue() {
         boolean result = user.validateEmail("brachanaBr@gmail.com");
         Assertions.assertTrue(result);
-    }
-    @Test
-    public void email_WhenFalse() {
+        }
+
+        @Test
+        public void email_WhenFalse() {
         boolean result = user.validateEmail("bracha.naBR^r@gmail^..com");
         Assertions.assertFalse(result);
         }
 
         @Test
         public void mobileNumber_WhenTrue() {
-            boolean result = user.validateMobileNumber("91 7892008230");
+            boolean result = user.validateMobilNum("91 7892008230");
             Assertions.assertTrue(result);
         }
-    @Test
-    public void mobileNumber_WhenFalse() {
-        boolean result = user.validateMobileNumber("123456789055");
+         @Test
+        public void mobileNumber_WhenFalse() {
+        boolean result = user.validateMobilNum("123456789055");
         Assertions.assertFalse(result);
-    }
-*/
+        }
 
         @Test
         public void Password_WhenTrue() {
-            boolean result = user.validatePassword("rachyR5*ll");
+            boolean result = user.validatePassword("Rachu(*^12345");
             Assertions.assertTrue(result);
         }
         @Test
@@ -57,8 +70,6 @@ public class UserRegisterationTest {
         boolean result = user.validatePassword("raggh9");
         Assertions.assertFalse(result);
         }
-
-
 }
 
 
